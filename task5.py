631ec1cdc58c7c6a59975c59f6abc5677ec4665d
Eletriclimbo <potapnev911@gmail.com>
2018-07-13 11:18:46 +0300

    Update hamowork2.2 Patapniou

--- task5.py
+++ task5.py
@@ -1,5 +1,4 @@
 from collections import Counter
 c = Counter(input()).most_common(3)
-b = sorted(c, key=lambda ff: (-ff[1], ff[0]))
 for x in range(3):
-    print(b[x][0], b[x][1])
+    print(c[x][0], c[x][1])
