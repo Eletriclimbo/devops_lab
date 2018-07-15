cbf9eb1beca4027059d79573653bb07842a93d1a
Eletriclimbo <32715736+Eletriclimbo@users.noreply.github.com>
2018-07-15 22:26:23 +0300

    Add files via upload
    6

--- Homework5.py
+++ Homework5.py
@@ -40,24 +40,24 @@ def get_getapi(username, rep):
 def take_day_opened(data):
     for k in data:
         print('User: ', k['user']['login'],
-              '\n Day opened:k6 ',
-              datetime.datetime.strptime(str(k["created_at"]),
+              '\n Day opened:k6 ',\
+              datetime.datetime.strptime(str(k["created_at"]),\
                                             '%Y-%m-%dT%H:%M:%SZ').strftime('%A'))
 
 
 def take_hour_opened(data):
     for k in data:
         print('User: ', k['user']['login'],
-              '\n Hour opened: ',
-              datetime.datetime.strptime(str(k["created_at"]),
+              '\n Hour opened: ',\
+              datetime.datetime.strptime(str(k["created_at"]),\
                                          '%Y-%m-%dT%H:%M:%SZ').strftime('%H'))
 
 
 def take_week_opened(data):
     for k in data:
-        print('User: ', k['user']['login'],
-              '\n Week opened: ',
-              datetime.datetime.strptime(str(k["created_at"]),
+        print('User: ', k['user']['login'],\
+              '\n Week opened: ',\
+              datetime.datetime.strptime(str(k["created_at"]),\
                                          '%Y-%m-%dT%H:%M:%SZ').strftime('%V'))
 
 
@@ -68,10 +68,10 @@ def user_who_opened(data):
 
 def number_of_days_opened(data):
     for k in data:
-        print('User: ', k['user']['login'],
-              '\n Number of days: ',
-              int(datetime.datetime.now().strftime('%j')) -
-              int(datetime.datetime.strptime(str(k["created_at"]),
+        print('User: ', k['user']['login'],\
+              '\n Number of days: ',\
+              int(datetime.datetime.now().strftime('%j')) -\
+              int(datetime.datetime.strptime(str(k["created_at"]),\
                     '%Y-%m-%dT%H:%M:%SZ').strftime('%j')))
 
 
