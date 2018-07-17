badbb2c7df3c8838d7a06c98350d65fcc8536f16
Eletriclimbo <32715736+Eletriclimbo@users.noreply.github.com>
2018-07-17 19:09:35 +0300

    Add files via upload

--- newtest.py
+++ newtest.py
@@ -9,8 +9,10 @@ class TestOlder(unittest.TestCase):
 
     def test_is_older_man(self):
         """Test for is _prime"""
-        self.assertEqual(new4.is_older_man([0, 0, 1, 1], [77, 100, 77, 25], 4), (1, 2))
-        self.assertEqual(new4.is_older_man([0, 0, 0, 0], [77, 100, 77, 25], 4), (0, 4))
+        self.assertEqual(new4.is_older_man([0, 0, 1, 1],
+                                           [77, 100, 77, 25], 4), (1, 2))
+        self.assertEqual(new4.is_older_man([0, 0, 0, 0],
+                                           [77, 100, 77, 25], 4), (0, 4))
 
     def tearDown(self):
         """Finish"""
